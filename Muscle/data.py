3e3a0753c2d30b84b202c8a0b0c9b0007e3e47ed
georgedimitriadis <gdimitri@hotmail.com>
2024-12-12 09:43:33 +0000

    Started the basic layout

---
+++ Muscle/data.py
@@ -0,0 +1,143 @@
+
+schedule = {'beginner': {
+    'stage 1': {
+        'week 1': {
+            'mini circuit 1': {
+                'name': ['THIN TUMMY', 'STATIC LUNGE', 'DUMBBELL LYING ROW', 'DUMBBELL UPRIGHT ROW',
+                              'CURLUP CHEATUP AND SLOW LOWER', 'DUMBBELL BENCH STEP', 'DUMBBELL LYING PULLOVER',
+                              'DUMBBELL BENCH PRESS', 'PUSHUP HOLD', 'STANDING SINGLE LEG CALF RAISE',
+                              'DUMBBELL SEATED HAMMER CURL WITH TWIST ALTERNATING', 'DUMBBELL TRICEPS KICKBACK'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+                'work reps': ['15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20',
+                              '15-20', '15-20'],
+                'tempo': ['5 sec hold', '311', '311', '311', '311', '311', '311', '311', '311', '311', '311', '311'],
+                'rest': ['30', '30', '30', '30', '30', '30', '30', '30', '30', '30', '30', '30']
+            }
+        },
+        'week 2': {
+            'mini circuit 1': {
+                'name': ['THIN TUMMY', 'STATIC LUNGE', 'DUMBBELL LYING ROW', 'DUMBBELL UPRIGHT ROW',
+                              'CURLUP CHEATUP AND SLOW LOWER', 'DUMBBELL BENCH STEP', 'DUMBBELL LYING PULLOVER',
+                              'DUMBBELL BENCH PRESS', 'PUSHUP HOLD', 'STANDING SINGLE LEG CALF RAISE',
+                              'DUMBBELL SEATED HAMMER CURL WITH TWIST ALTERNATING', 'DUMBBELL TRICEPS KICKBACK'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+                'work reps': ['15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20',
+                              '15-20', '15-20'],
+                'tempo': ['5 sec hold', '311', '311', '311', '311', '311', '311', '311', '311', '311', '311', '311'],
+                'rest': ['30', '30', '30', '30', '30', '30', '30', '30', '30', '30', '30', '30']
+            }
+        },
+        'week 3': {
+            'mini circuit 1': {
+                'name': ['THIN TUMMY', 'STATIC LUNGE', 'DUMBBELL LYING ROW', 'DUMBBELL UPRIGHT ROW',
+                              'CURLUP CHEATUP AND SLOW LOWER', 'DUMBBELL BENCH STEP', 'DUMBBELL LYING PULLOVER',
+                              'DUMBBELL BENCH PRESS', 'PUSHUP HOLD', 'STANDING SINGLE LEG CALF RAISE',
+                              'DUMBBELL SEATED HAMMER CURL WITH TWIST ALTERNATING', 'DUMBBELL TRICEPS KICKBACK'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+                'work reps': ['15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20', '15-20',
+                              '15-20', '15-20'],
+                'tempo': ['5 sec hold', '311', '311', '311', '311', '311', '311', '311', '311', '311', '311', '311'],
+                'rest': ['30', '30', '30', '30', '30', '30', '30', '30', '30', '30', '30', '30']
+            }
+        },
+        'week 4': {
+            'mini circuit 1': {
+                'name': ['THIN TUMMY LIFT ONE LEG', 'STATIC LUNGE', 'DUMBBELL LYING ROW', 'DUMBBELL UPRIGHT ROW'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1', '1', '1', '1'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['313', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60']
+            },
+            'mini circuit 2':{
+                'exercise': ['CURLUP ARTMS STRAIGHT AND PARALLEL TO FLOOR', 'DUMBBELL BENCH STEP', 'DUMBBELL LYING PULLOVER',
+                              'DUMBBELL BENCH PRESS'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1', '1', '1', '1'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['515', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60 - 120']
+            },
+            'mini circuit 3':{
+                'exercise': ['PUSHUP HOLD HANDS AND FEET', 'STANDING SINGLE LEG CALF RAISE',
+                              'DUMBBELL SEATED HAMMER CURL WITH TWIST ALTERNATING', 'DUMBBELL TRICEPS KICKBACK'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1', '1', '1', '1'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['311', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60 - 120']
+            }
+        },
+        'week 5': {
+            'mini circuit 1': {
+                'name': ['THIN TUMMY LIFT ONE LEG', 'STATIC LUNGE', 'DUMBBELL LYING ROW', 'DUMBBELL UPRIGHT ROW'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1 or 2', '1 or 2', '1 or 2', '1 or 2'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['313', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60']
+            },
+            'mini circuit 2':{
+                'exercise': ['CURLUP ARTMS STRAIGHT AND PARALLEL TO FLOOR', 'DUMBBELL BENCH STEP', 'DUMBBELL LYING PULLOVER',
+                              'DUMBBELL BENCH PRESS'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1 or 2', '1 or 2', '1 or 2', '1 or 2'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['515', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60 - 120']
+            },
+            'mini circuit 3':{
+                'exercise': ['PUSHUP HOLD HANDS AND FEET', 'STANDING SINGLE LEG CALF RAISE',
+                              'DUMBBELL SEATED HAMMER CURL WITH TWIST ALTERNATING', 'DUMBBELL TRICEPS KICKBACK'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['1 or 2', '1 or 2', '1 or 2', '1 or 2'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['311', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60 - 120']
+            }
+        },
+        'week 6': {
+            'mini circuit 1': {
+                'name': ['THIN TUMMY LIFT ONE LEG', 'STATIC LUNGE', 'DUMBBELL LYING ROW', 'DUMBBELL UPRIGHT ROW'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['2 or 3', '2 or 3', '2 or 3', '2 or 3'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['313', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60']
+            },
+            'mini circuit 2':{
+                'exercise': ['CURLUP ARTMS STRAIGHT AND PARALLEL TO FLOOR', 'DUMBBELL BENCH STEP', 'DUMBBELL LYING PULLOVER',
+                              'DUMBBELL BENCH PRESS'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['2 or 3', '2 or 3', '2 or 3', '2 or 3'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['515', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60 - 120']
+            },
+            'mini circuit 3':{
+                'exercise': ['PUSHUP HOLD HANDS AND FEET', 'STANDING SINGLE LEG CALF RAISE',
+                              'DUMBBELL SEATED HAMMER CURL WITH TWIST ALTERNATING', 'DUMBBELL TRICEPS KICKBACK'],
+                'warmup sets': [],
+                'warmpup reps': [],
+                'work sets': ['2 or 3', '2 or 3', '2 or 3', '2 or 3'],
+                'work reps': ['10-15', '10-15', '10-15', '10-15'],
+                'tempo': ['311', '311', '311', '311'],
+                'rest': ['60', '60', '60', '60 - 120']
+            }
+        }
+    },
+}}
